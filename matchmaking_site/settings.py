e0adfa13a4563e52722ab62c0ebc957c7e58d817
n.annalingam@se16.qmul.ac.uk <n.annalingam@se16.qmul.ac.uk>
2018-12-14 15:59:45 +0000

    Fixed Heroku

--- matchmaking_site/settings.py
+++ matchmaking_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,4 +136,4 @@ EMAIL_HOST_PASSWORD = "SG.C-TuJG5QRwqlG1N9LJqqYA.sfNkKF-mjUO5FK3JEVJRC6iMEGWpCSJ
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
